bd784c25f7d9aa9e435aada19d33f00164f91b3b
yichen <yzhao179@ucsc.edu>
2021-01-24 23:25:19 -0800

    aaa

---
+++ src/components/CardSample.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Form, Button, Card, Alert} from "react-bootstap";
+
+function cardSample() {
+    return (
+
+    <Card></Card>
+
+    );
+}
+
+//export default ClassSearchPage;
