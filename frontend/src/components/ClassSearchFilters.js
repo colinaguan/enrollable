55ec45da02866d63675ae732638206961ba4e611
Colina Guan <colinaguan@Colinas-MBP.attlocal.net>
2021-02-02 21:52:58 -0800

    finished class search filters

--- frontend/src/components/ClassSearchFilters.js
+++ frontend/src/components/ClassSearchFilters.js
@@ -1,126 +1,59 @@
-import React from 'react';
-import { Form, Button, ButtonGroup, Col, Container, Dropdown, DropdownButton, Row } from "react-bootstrap";
+import React, { useState } from 'react';
+import { Form, Button, Col } from "react-bootstrap";
 import '../style/ClassSearchFilters.css';
-/*To add more eventkey to the dropdown box, use <Dropdown.Item eventKey="1">1</Dropdown.Item>*/
 
 function ClassSearchFilters() {
-    const handleSubmit = (event) => {
-        const form = event.currentTarget;
+
+    const [department, setDep] = useState('any');
+    const [ge, setGE] = useState('any');
+    const [type, setType] = useState('any');
+    const [favorites, setFav] = useState('any');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("---------- FILTERS");
+        console.log(department);
+        console.log(ge);
+        console.log(type);
+        console.log(favorites);
     };
     
     return (
-        <Form className='filter-form'>
+        <Form className='filter-form' onSubmit={handleSubmit}>
             <Form.Row>
                 <Form.Group as={Col} sm={3} controlId="formDepartment">
                     <Form.Label>Department</Form.Label>
-                    <Form.Control as="select">
-                        <option>Any Department</option>
+                    <Form.Control as="select" value={department} onChange={(e) => setDep(e.target.value)}>
+                        <option value='any'>Any Department</option>
+                        <option>CSE</option>
                     </Form.Control>
                 </Form.Group>
                 <Form.Group as={Col} sm={2} controlId="formGE">
                     <Form.Label>GE</Form.Label>
-                    <Form.Control as="select">
-                        <option>None</option>
+                    <Form.Control as="select" value={ge} onChange={(e) => setGE(e.target.value)}>
+                        <option value='any'>Any GE</option>
                     </Form.Control>
                 </Form.Group>
                 <Form.Group as={Col} sm={3} controlId="formClassType">
                     <Form.Label>Class Type</Form.Label>
-                    <Form.Control as="select">
-                        <option>Any Class Type</option>
+                    <Form.Control as="select" value={type} onChange={(e) => setType(e.target.value)}>
+                        <option value='any'>Any Class Type</option>
                     </Form.Control>
                 </Form.Group>
                 <Form.Group as={Col} sm={3} controlId="formFavorites">
                     <Form.Label>Favorites</Form.Label>
-                    <Form.Control as="select">
-                        <option>All Classes</option>
+                    <Form.Control as="select" value={favorites} onChange={(e) => setFav(e.target.value)}>
+                        <option value='any'>All Classes</option>
                         <option>My Favorites</option>
                     </Form.Control>
                 </Form.Group>
                 <Form.Group as={Col} sm={1} className='filters-button' controlId="formFavorites">
-                    <Button variant="primary" type="submit">
+                    <Button variant="purple" type="submit">
                         Search
                     </Button>
                 </Form.Group>
             </Form.Row>
         </Form>
-    );
-}
-
-function Dropdowns() {
-    return (
-        <Container>
-            <Row className="justify-content-lg-center">
-                {['Department'].map((idx)=>(
-                    <>
-                        <Col md="auto">{idx}
-                            <p>
-                                <DropdownButton
-                                    as={ButtonGroup}
-                                    key={idx}
-                                    id="dropdown-${idx}"
-                                    variant="light"
-                                    title={idx}
-                                >
-                                    <Dropdown.Item eventKey="1">1</Dropdown.Item>
-                                </DropdownButton>
-                            </p>
-                        </Col>
-                    </>
-                ))}
-                {['GE'].map((idx)=>(
-                    <>
-                        <Col md="auto">{idx}
-                            <p>
-                                <DropdownButton
-                                    as={ButtonGroup}
-                                    key={idx}
-                                    id="dropdown-${idx}"
-                                    variant="light"
-                                    title={idx}
-                                >
-                                    <Dropdown.Item eventKey="1">1</Dropdown.Item>
-                                </DropdownButton>
-                            </p>
-                        </Col>
-                    </>
-                ))}
-                {['ClassType'].map((idx)=>(
-                    <>
-                        <Col md="auto">{idx}
-                            <p>
-                                <DropdownButton
-                                    as={ButtonGroup}
-                                    key={idx}
-                                    id="dropdown-${idx}"
-                                    variant="light"
-                                    title={idx}
-                                >
-                                    <Dropdown.Item eventKey="1">1</Dropdown.Item>
-                                </DropdownButton>
-                            </p>
-                        </Col>
-                    </>
-                ))}
-                {['Favorites'].map((idx)=>(
-                    <>
-                        <Col md="auto">{idx}
-                            <p>
-                                <DropdownButton
-                                    as={ButtonGroup}
-                                    key={idx}
-                                    id="dropdown-${idx}"
-                                    variant="light"
-                                    title={idx}
-                                >
-                                    <Dropdown.Item eventKey="1">1</Dropdown.Item>
-                                </DropdownButton>
-                            </p>
-                        </Col>
-                    </>
-                ))}
-                <Col sm={1}><Button>Search</Button></Col>
-            </Row>
-        </Container>
     );
 }
 
