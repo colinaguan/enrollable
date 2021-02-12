7ac0dc2209f21cdacdb022b731eba85ea2b1259d
Brenden Greenlee <bjgreenl@ucsc.edu>
2021-02-12 12:31:00 -0800

    Added more items to class object, added mapping of instructor name to Rate My Professor id number

--- backend/routes/db.js
+++ backend/routes/db.js
@@ -6,6 +6,7 @@ var json = require('json');
 var classData = {};
 var items = {};
 var items2 = {};
+var rmpMap = {};
 
 const getData=()=>{
     console.log("get data start");
@@ -24,17 +25,54 @@ const getData2=()=>{
   .then(res => res.json())
   .then(json => {
     items2 = json;
+    getRmpMap();
     console.log('get data completed');
     //after all data has been received
     //parse data into our new javascript object
+    //parsedata();
+  });
+}
+
+//Get mapping of prof FirstnameLastname to rate my proffessor id
+const getRmpMap=()=>{
+  fetch('https://andromeda.miragespace.net/slugsurvival/data/fetch/rmp.json')
+  .then(res => res.json())
+  .then(json => {
+    rmpMap = json;
+    //console.timeEnd('parse');
     parsedata();
-  })
+    //console.log(rmpMap);  
+  });
+}
+
+/*
+  //Old attempt at getting rate my professor data for each class
+  //Because fetch is slow this will take a huge amount of time
+  //Because fetch is also asychnronous there are additional problems with using it 1,300 times
+  //in the middle of parsing a bunch of data
+const getRmpInfo=(id)=>{
+  fetch('https://andromeda.miragespace.net/slugsurvival/data/fetch/rmp/scores/' + id)
+  .then(res => res.json())
+  .then(json => {
+    json = {};
+    //console.log(json);
+    return json;
+  });
+}
+*/
+
+const getRmpInfo2 = async (rmpId) => {
+  //var temp = {};
+  const ret = await fetch('https://andromeda.miragespace.net/slugsurvival/data/fetch/rmp/scores/' + rmpId);
+  const json = await ret.json();
+  console.log(json);
 }
 
 //get first json file from slugsurvival
 getData();
 
 function parsedata() {
+  var test = true;
   console.log("Started parse data");
   //for each department create a new empty object
   Object.entries(items).map(department => {
@@ -46,6 +84,7 @@ function parsedata() {
     Object.entries(department[1]).map(course => {
       var newClass = {};
       newClass.code = course[1].c;
+      newClass.clasSection = course[1].s;
       newClass.name = course[1].n;
       newClass.num = course[1].num;
       newClass.dep = department[0];
@@ -62,6 +101,19 @@ function parsedata() {
         newClass.instructor = '';
       }
 
+      if (course[1].ins.f != null && course[1].ins.l != null){
+        var temp = course[1].ins.f + course[1].ins.l;
+        for (var fl in rmpMap){
+          if (fl === temp){
+            newClass.instructorId = rmpMap[fl];
+            //console.log(getRmpInfo(newClass.instructorId));
+            break;
+          }
+        }
+      } else {
+        newClass.instructorId = '';
+      }
+
       if (course[1].loct[0].t != null && typeof(course[1].loct[0].t.day) != 'undefined'){
         newClass.day = course[1].loct[0].t.day;
       } else {
@@ -76,12 +128,15 @@ function parsedata() {
         newClass.end = '';
       }
 
+      //var printone = true;
       for (var i in items2){
         if (i == newClass.num){
           newClass.ge = items2[i].ge;
+          newClass.description = items2[i].desc;
+          newClass.requirements = items2[i].re;
           newClass.type = items2[i].ty;
           newClass.credits = parseInt(items2[i].cr);
-          newClass.section = [];
+          newClass.sections = [];
 
           for (var j in items2[i].sec){
             var newSection = {};
@@ -89,7 +144,6 @@ function parsedata() {
             newSection.num = items2[i].sec[j].num;
             newSection.instructor = items2[i].sec[j].ins;
             
-
             if (items2[i].sec[j].loct[0].t != null && 
                 typeof(items2[i].sec[j].loct[0].t.time) != 'undefined'){
                   newSection.start = items2[i].sec[j].loct[0].t.time.start;
@@ -108,12 +162,12 @@ function parsedata() {
             
             if (items2[i].sec[j].loct[0].t != null && 
                 typeof(items2[i].sec[j].loct[0].loc) != 'undefined'){
-                  newSection.loc = items2[i].sec[j].loct[0].loc;
+                  newSection.location = items2[i].sec[j].loct[0].loc;
             } else {
-              newSection.loc = '';
+              newSection.location = '';
             }
              
-            newClass.section.push(newSection);
+            newClass.sections.push(newSection);
 
           }
           if (newClass.type){
