93914d587bc1ca93f701781a9b3e50ac1973a98c
Colina Guan <colinaguan@Colinas-MBP.attlocal.net>
2021-02-16 19:53:55 -0800

    skeleton for class card and sections modal

--- frontend/src/components/GenerateClassCard.js
+++ frontend/src/components/GenerateClassCard.js
@@ -1,20 +1,64 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Card, Row, Col } from 'react-bootstrap';
+import { shortenDays, timeToString } from '../utils/format';
+import GenerateSectionsModal from './GenerateSectionsModal';
 
 function GenerateClassCard({ classNum, handleSelectedClasses }) {
 
-    // get data from API
-    fetch('course/course=' + classNum)
-    .then(res => res.json())
-    .then(data => {
-        console.log(data);
-    })
-    .catch(() => {
-        console.error("classNum API call not responding")
-        return;
-    });
+    const [classData, setClassData] = useState({});
+
+    const [show, setShow] = useState(false);
+    const handleShow = () => setShow(true);
+
+    useEffect(() => {
+        // get data from API
+        fetch('course/course=' + classNum)
+        .then(res => res.json())
+        .then(data => {
+            setClassData(data);
+        })
+        .catch(() => {
+            console.error("classNum API call not responding")
+            return;
+        });       
+    }, [classNum]);
+
+    // check if data has been updated
+    if (classData['num']) {
+        // class title (ex: CSE 101-01)
+        var classTitle = classData['dep'].toUpperCase() + ' ' + classData['code'];
+        if (classData['classSection'] !== '01') classTitle += '-' + classData['classSection'];
+        // class day time info
+        var classDay = shortenDays(classData['day']);
+        var classStart = timeToString(classData['start']);
+        var classEnd = timeToString(classData['end']);
+        var classDayTime = (classDay && classStart && classEnd) ?
+            classDay + ' ' + classStart + ' - ' + classEnd :
+            '';
+    }
 
     return (
-        <p>ClassCard</p>
+        <Card className="class-card">
+            <Card.Body className='class-card-body'>
+                <Row>
+                    <Col sm={10}>
+                        <Card.Title>
+                            <b>{classTitle}</b> {classData['name']}
+                        </Card.Title>
+                        <Card.Text>
+                            {classDayTime !== '' && classDayTime}
+                            {classDayTime === '' && <i>No listed day or time</i>}
+                        </Card.Text>
+                        <Card.Link onClick={handleShow} className='more-class-info'>
+                            Choose sections...
+                        </Card.Link>
+                    </Col>
+                    <Col sm={2} className='star-container'>
+                    </Col>
+                </Row>
+            </Card.Body>
+            <GenerateSectionsModal classData={classData} show={show} setShow={setShow}/>
+        </Card>
     );
 }
 
--- frontend/src/components/GenerateSectionsModal.js
+++ frontend/src/components/GenerateSectionsModal.js
@@ -1,8 +1,76 @@
 import React from 'react';
+import { Modal, Container, Row, Col, Card } from 'react-bootstrap';
+import { shortenDays, timeToString } from '../utils/format';
+import '../style/ClassSearchModal.css';
 
-function GenerateSectionsModal() {
+function GenerateSectionsModal({ classData, show, setShow }) {
+    const handleClose = () => setShow(false);
+
+    // check if classData has been updated
+    if (classData['num']) {
+        
+        // class title (ex: CSE 101-01)
+        var classTitle = classData['dep'].toUpperCase() + ' ' + classData['code'];
+        if (classData['classSection'] !== '01') classTitle += ('-' + classData['classSection']);
+
+        // map section info
+        var sections = classData['sections'].map(data => {
+            var secDay = shortenDays(data['day']);
+            var secStart = timeToString(data['start']);
+            var secEnd = timeToString(data['end']);
+            var secDayTime = (secDay && secStart && secEnd) ?
+                secDay + ' ' + secStart + ' - ' + secEnd :
+                '';
+            return (
+                <Row key={data['num']} className='row-bottom-pad'>
+                    <Col sm={4}>
+                        {classTitle}-{data['secName']}
+                    </Col>
+                    <Col sm={4}>
+                        {secDayTime !== '' && secDayTime}
+                        {secDayTime === '' && <i>Not stated</i>}
+                    </Col>
+                    <Col sm={4}>
+                        {data['instructor']}
+                    </Col>
+                </Row>
+            )
+        });
+    }
+
     return (
-        <p>Sections Modal</p>
+        <Modal show={show} onHide={handleClose} animation={false} dialogClassName="info-modal">
+            <Modal.Header closeButton>
+                <Modal.Title>
+                    <b>{classTitle}</b> {classData['name']}
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Container>
+                    <Row>
+                        <h5 className='section-title'>Sections</h5>
+                    </Row>
+                    <Row>
+                        <Card className='info-card'>
+                            <Card.Body>
+                                <Container>
+                                    <Row>
+                                        <Col>
+                                            <Row className='row-bottom-pad'>
+                                                <Col className='info-title' sm={4}>Section</Col>
+                                                <Col className='info-title' sm={4}>Day and Time</Col>
+                                                <Col className='info-title' sm={4}>Instructor</Col>
+                                            </Row>
+                                            {sections}
+                                        </Col>
+                                    </Row>
+                                </Container>
+                            </Card.Body>
+                        </Card>
+                    </Row>
+                </Container>
+            </Modal.Body>
+        </Modal>
     );
 }
 
