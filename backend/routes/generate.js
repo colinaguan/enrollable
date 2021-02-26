0ff5c9d65467464944878a5554a5c09c8690e752
DiNa Chen <DiNa@DiNadeMacBook-Pro.local>
2021-02-26 11:41:05 -0800

    fixed generate algorithm, still has issue

--- backend/routes/generate.js
+++ backend/routes/generate.js
@@ -92,17 +92,53 @@ function getNoConflictSchedules(classesList) {
     // go through all courses except the last course
     for (var i=0; i<classesList.length-1; i++) {
         var course = classesList[i];
+        console.log("course root")
+        console.log(course)
         var newStack = [];
-        newStack.push([course]);
-        while (!newStack.length) {
+        newStack.push(course);// newStack.push([course]);
+        console.log("stack")
+        console.log(newStack)
+        while (newStack.length) { //
+            console.log("enter")
             schedule = newStack.pop();
-            for (newClass in successor(schedule, classesList)) {
-                if (!checkTimeConflict(schedule, newClass)) {
-                    schedule.append(newClass);
-                    possibleSchedules.append(schedule);
-                    newStack.push(schedule);
-                }
+            console.log("pop")
+            console.log(schedule)
+            successors = successor(schedule, classesList)
+            if(successors.length !== 0){
+                successors.forEach(newClass=>{
+                    console.log("newClass")
+                    console.log(newClass)
+                    // conflict = true
+                    conflict = checkTimeConflict(schedule, newClass) // always return false? even the function returns true
+                    console.log("conflict", conflict)
+                    if (!conflict) {
+                        console.log("no conflict pass")
+                        if(typeof(schedule) === 'object') { //case when schdule is a single course
+                            console.log("object?")
+                            schedule = [schedule]
+                        }
+                        schedule.push(newClass);
+                        console.log("schedule")
+                        console.log(schedule)
+                        possibleSchedules.push(schedule);
+                        newStack.push(schedule);
+                    }
+                    console.log("possible schdule", possibleSchedules)
+    
+    
+                })
+                
             }
+            
+            // for (newClass in successor(schedule, classesList)) {
+            //     console.log("newClass")
+            //     console.log(newClass)
+            //     if (!checkTimeConflict(schedule, newClass)) {
+            //         schedule.append(newClass);
+            //         possibleSchedules.append(schedule);
+            //         newStack.push(schedule);
+            //     }
+            // }
         }	
     }
 
@@ -110,38 +146,71 @@ function getNoConflictSchedules(classesList) {
 }
 
 function successor(schedule, classesList) {
-	lastCourse = schedule[schedule.length-1];
+    console.log("length")
+    // console.log(schedule)  printed not an array
+    console.log(schedule.length)
+    console.log(typeof(schedule))
+    // note: can't use type of(), because it will return "object" for array of objects 
+    if(!Array.isArray(schedule)) {// if(typeof(schedule) === 'object') { //case when schdule is a single course
+        console.log("not array")
+        index = 0
+        lastCourse = schedule
+    }else{
+        console.log("is array")
+        index = schedule.length-1
+        lastCourse = schedule[index];
+    }
+    
+    console.log("last course")
+    console.log(lastCourse)
 	successors = [];
-	for (var i=selectedList.indexOf(lastCourse)+1; i<classesList.length; i++) {
-		if (i >= selectedList.length) {
+	for (var i=classesList.indexOf(lastCourse)+1; i<classesList.length; i++) {
+        console.log("i",i)
+        if (i >= classesList.length) {
             return successors; 
         }
-        successors.append(classesList[i]);
+
+        successors.push(classesList[i]);
     }
+    console.log("successors");
+    console.log(successors);
 	return successors;
 
 }
 
 function checkTimeConflict(schedule, newClass) {
-    for (classData in schedule) {
+    // forEach(newClass=>{
+    if(typeof(schedule) === 'object') { //case when schdule is a single course
+        schedule = [schedule]
+    }
+    schedule.forEach(classData=>{
         classDate = classData.days;
         courseDate = newClass.days;
         intersectDate = intersect(classDate, courseDate);
-        if (len(intersectDate) === 0) {
-            continue;
-        }
-        // check overlap
-        if (classData.start<=newClass.end && classData.end>=newClass.start){
-            // if the conflict pair was not recorded, append to the list
-            if (conflictPair.indexOf((newClass, classData)) === -1) {
-                conflictPair.append((newClass, classData));
+        console.log("intersect", intersectDate)
+        if (intersectDate.length !== 0) {
+             // check overlap
+            console.log("classData.start", classData.start)
+            console.log("newClass.end", newClass.end)
+            console.log("-")
+            console.log("classData.end", classData.end)
+            console.log("newClass.start", newClass.start)
+            console.log("classData.start<=newClass.end",classData.start<=newClass.end)
+            console.log("classData.end>=newClass.start", classData.end>=newClass.start)
+            if (classData.start<=newClass.end && classData.end>=newClass.start){
+                console.log("detected conflcit")
+                // if the conflict pair was not recorded, append to the list
+                if (conflictPairs.indexOf((newClass, classData)) === -1) {
+                    conflictPairs.push((newClass, classData));
+                }
+                console.log("conflictPairs",conflictPairs)
+                return true;
             }
-
-            return true;
         }
-    }
+       
 
-   return false;
+    })
+    return false;
 }
 
 function conflictAvoidTime(avoidTime, newClass) {
@@ -153,12 +222,20 @@ function conflictAvoidTime(avoidTime, newClass) {
             continue;
         }
         // check overlap
+
         if (avoidTimeDates.start<=newClass.end && avoidTimeDates.end>=newClass.start){
             return true;
         }
     }
 
     return false;
+}
+
+function intersect(arrA, arrB) {
+    console.log("arrA", arrA)
+    console.log("arrB", arrB)
+    let intersection = arrA.filter(x => arrB.includes(x));
+    return intersection
 }
 
 module.exports = router;
