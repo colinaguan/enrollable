74c330b8fde1420aecfd7fc546afecec207fe81c
Colina Guan <cobguan@ucsc.edu>
2021-03-09 15:29:10 -0800

    added hook for schedule cards

--- frontend/src/components/SavedSchedulesPage.js
+++ frontend/src/components/SavedSchedulesPage.js
@@ -3,107 +3,30 @@ import { Container, Row } from 'react-bootstrap';
 import SavedScheduleCard from './SavedScheduleCard';
 import  { useState, useEffect } from 'react';
 import { useAuth } from "../contexts/AuthContext";
+
 function SavedSchedulesPage() {
-    // {savedSchedules}
-    // var schedules = [];
+
     const [schedules, setSchedules] = useState([]);
+    const [cards, setCards] = useState([]);
 
     const {getSavedSchedules} = useAuth();
 
-    useEffect( () =>{
-        setSchedules(getSavedSchedules());
+    useEffect( () => {
+        const newScheds = getSavedSchedules();
+        setSchedules(newScheds);
+        var cards = newScheds.map((schedule, index) => {
+            return (
+              <SavedScheduleCard
+                key={index}
+                id={index}
+                title={schedule.title}
+                description={schedule.description}
+                classes={schedule.classes}
+               />
+            );
+        });
+        setCards(cards);
     }, []);
-    //var schedules = getSavedSchedules();
-
-    /*
-    var savedSchedules= [
-        {
-            "title": "backup",
-            "description": "good",
-            "classes": [
-                {
-                    "num": 12345, // for key?
-                    "title": "CSE 101",
-                    "dayTime": "TuTh 1:30PM - 3:05PM",
-                    "sections": [
-                        {
-                            "num": 23456,
-                            "title": "1A",
-                            "dayTime": "TuTh 1:30PM - 3:05PM"
-                        }
-     
-                    ]
-                }, 
-                {
-                    "num": 22345, 
-                    "title": "CSE 102",
-                    "dayTime": "Mon 1:30PM - 3:05PM",
-                    "sections": [
-                        {
-                            "num": 33456,
-                            "title": "1A",
-                            "dayTime": "TuTh 1:30PM - 3:05PM"
-                        },
-                        {
-                            "num": 33457,
-                            "title": "1B",
-                            "dayTime": "TuTh 1:30PM - 3:05PM"
-                        }
-     
-                    ]
-                },
-            ]
-        },
-        {
-            "title": "optimal",
-            "description": "perfect",
-            "classes": [
-                {
-                    "num": 12345, // for key?
-                    "title": "CSE 101",
-                    "dayTime": "TuTh 1:30PM - 3:05PM",
-                    "sections": [
-                        {
-                            "num": 23456,
-                            "title": "1A",
-                            "dayTime": "TuTh 1:30PM - 3:05PM"
-                        }
-     
-                    ]
-                }, 
-                {
-                    "num": 22345, 
-                    "title": "CSE 102",
-                    "dayTime": "Mon 1:30PM - 3:05PM",
-                    "sections": [
-                        {
-                            "num": 33456,
-                            "title": "1A",
-                            "dayTime": "TuTh 1:30PM - 3:05PM"
-                        }
-     
-                    ]
-                },
-            ]
-        }
-    ]
-    */
-    
-    // const [scheduleCards, setCards] = useState([]);
-    var cards = schedules.map((schedule, index) => {
-        
-        return (
-          <SavedScheduleCard
-            key={index}
-            id={index}
-            title={schedule.title}
-            description={schedule.description}
-            classes={schedule.classes}
-           />
-        );
-    });
-    
-    // setCards(cards);
 
     return (
         <Container>
@@ -114,7 +37,7 @@ function SavedSchedulesPage() {
                 { 
                     cards && cards.length > 0 ? 
                     cards :
-                    <p><i>No schedules to display. Go generate some schedules:)</i></p>
+                    <p><i>No schedules to display. Go generate some schedules :)</i></p>
                 }
             </Row>
         </Container>
